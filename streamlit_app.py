87d45efde2ce30141e12f32b8084e5c21cd12845
yellowpowerring <150839037+yellowpowerring@users.noreply.github.com>
2023-11-14 12:46:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-Streamlit.text('Kale, Spinach & Rocket Smoothie')
-Streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
