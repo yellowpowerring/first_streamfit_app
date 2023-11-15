1cdff394d587250b6209d11d67bcfc77aec5c299
yellowpowerring <150839037+yellowpowerring@users.noreply.github.com>
2023-11-15 16:37:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ try:
     # write your own comment - what does this do?
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-streamlit.error()
+  streamlit.error()
 
 streamlit.stop()
 
